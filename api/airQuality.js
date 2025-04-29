b2c4c4101b6c6a2dce458ae6bfd7355e47ead321
Andrey <andrei9me@gmail.com>
2025-04-29 20:11:23 +0300

    Try

--- api/airQuality.js
+++ api/airQuality.js
@@ -1,17 +1,14 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   const { city } = req.query;
   const token = process.env.API_TOKEN;
-
   const apiUrl = `https://api.waqi.info/feed/${city}/?token=${token}`;
 
   try {
     const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error("Network response was not ok " + response.statusText);
-    }
+    if (!response.ok) throw new Error("Network response was not ok");
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: error.toString() });
   }
-};
+}
